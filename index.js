b7978a34ac5116cf2b432577f566ebe84a30a755
webfaisalbd <web.faisal.bd@gmail.com>
2022-07-28 17:24:49 +0600

    mvc architecture done with users

--- index.js
+++ index.js
@@ -1,48 +1,14 @@
 const express = require('express');
 
+const userRouter = require("./routes/users.route")
+
 const app = express();
 const PORT = 3000;
 
 // without body-parser
 app.use(express.urlencoded({extended: true}));
 
-const users = [
-    {
-        name: "Faisal",
-        age: 25
-    },
-    {
-        name: "Ahmed",
-        age: 26
-    }
-]
-
-const htmlForm = `
-    <form method="POST" action="/users">
-        <input type="text" name="name" placeholder="Enter your name" />
-        <input type="number" name="age" placeholder="Enter your age" />
-        <button type="submit">Save Data</button>
-    </form>
-`;
-
-
-app.get('/users', (req, res) => {
-    res.send(htmlForm);
-});
-
-app.post('/users', (req, res) => {
-    const name = req.body.name;
-    const age = Number(req.body.age);
-    const user = {
-        name, 
-        age
-    };
-    users.push(user);
-    res.status(201).json({
-        success: true,
-        users
-    })
-});
+app.use(userRouter);
 
 app.use((req,res,next)=>{
     res.status(404).json({
---
+++ models/users.model.js
@@ -0,0 +1,12 @@
+const users = [
+    {
+        name: "Faisal",
+        age: 25
+    },
+    {
+        name: "Ahmed",
+        age: 26
+    }
+]
+
+module.exports = users; 
